bcae78dea81df2ab67a53996ec85d1a01ab14709
edrick27 <edricklpcr@gmail.com>
2020-02-10 19:59:12 -0600

    change mounts

--- statistic.php
+++ statistic.php
@@ -7,10 +7,10 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include_once './dbclass.php';
 
-    $dbclass = new Database();
-    $connection = $dbclass->getConnection();
+$dbclass = new Database();
+$connection = $dbclass->getConnection();
 
-    $query = "SELECT 
+$query = "SELECT 
                     ft.id AS `id`,
                     ft.name AS `name`,
                     ft.color AS `color`,
@@ -29,60 +29,59 @@ include_once './dbclass.php';
                         GROUP BY funds.type) AS f
                     ON f.type = ft.id";
 
-    $stmt = $connection->query($query);
+$stmt = $connection->query($query);
 
-    $count = $stmt->num_rows;
+$count = $stmt->num_rows;
 
-    if($count > 0){
+if ($count > 0) {
 
 
-        $response = array();
-        $response["body"] = array();
+    $response = array();
+    $response["body"] = array();
 
-        while ($row = $stmt->fetch_assoc()){
+    while ($row = $stmt->fetch_assoc()) {
 
-            extract($row);
+        extract($row);
 
-            $p  = array(
-                "id" => $id,
-                "name" => $name,
-                "color" => $color,
-                "icon" => $icon,
-                "total" => $total,
-                "total_type" => $total_type,
-                "porcent" => is_null($porcent) ? "0%" : floor($porcent) . "%" ,
-            );
+        $p  = array(
+            "id" => $id,
+            "name" => $name,
+            "color" => $color,
+            "icon" => $icon,
+            "total" => $total,
+            "total_type" => $total_type,
+            "porcent" => is_null($porcent) ? "0%" : floor($porcent) . "%",
+        );
 
-            array_push($response["body"], $p);
-        }
+        array_push($response["body"], $p);
+    }
 
-        $response["general"] = array();
+    $response["general"] = array();
 
-        $query = "SELECT
+    $query = "SELECT
                     SUM(funds.money) AS `raised`,
                     ((SUM(funds.money) / 250000) * 100)  AS `funded`,
                     COUNT(DISTINCT funds.sponsor)  AS `sponsors`
                 FROM funds";
 
-        $stmt = $connection->query($query);
-        setlocale(LC_MONETARY, 'en_US');
-        while ($row = $stmt->fetch_assoc()){
+    $stmt = $connection->query($query);
+    setlocale(LC_MONETARY, 'en_US');
+    while ($row = $stmt->fetch_assoc()) {
 
-            extract($row);
-            
-            $p  = array(
-                "raised" => "$" . number_format( $raised, 0 ),
-                "funded" => is_null($funded) ? "0%" : floor($funded) . "%" ,
-                "sponsors" => $sponsors,
-            );
+        extract($row);
 
-            array_push($response["general"], $p);
-        }
-
-        echo json_encode($response);
-    } else {
-        echo json_encode(
-            array("body" => array(), "general" => array())
+        $p  = array(
+            "raised" => "$" . number_format($raised, 0),
+            "funded" => is_null($funded) ? "0%" : floor($funded) . "%",
+            "sponsors" => $sponsors,
         );
+
+        array_push($response["general"], $p);
     }
-?>
+
+    echo json_encode($response);
+} else {
+    echo json_encode(
+        array("body" => array(), "general" => array())
+    );
+}
